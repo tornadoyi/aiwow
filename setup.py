8716416adbb2b40cef4b3f5aaf9131b6700fa52a
guyi.1016 <guyi.1016@bytedance.com>
2021-04-08 22:55:25 +0800

    remove read wowo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="aiwow",
     description="The AI of world of warcraft",
     author="Jason Ku",
-    packages=[p for p in find_packages() if p.startswith('aiwow') or p.startswith('readwow')],
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'aiwow = aiwow.cli:main',
